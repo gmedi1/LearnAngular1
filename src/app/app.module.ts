f5a75712ef80ad504982a071825f0db4a41c1108
prodigy9 <30520183+prodigy9@users.noreply.github.com>
2017-12-22 15:34:14 -0800

    Done. Angular code can be use in html code when enclosed in ''

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { Assignment2Component } from './assignment2/assignment2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    Assignment2Component
   ],
   //added FormsModule and HttpModule to be in sync with tutorial
   imports: [
